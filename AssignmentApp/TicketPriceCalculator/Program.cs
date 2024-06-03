a5e32ae459db22d9534eb1fe98b2168315733250
unknown <efsomuah@st.ug.edu.gh>
2024-06-02 18:58:44 -0700

    Task 2: Added Ticket Price Calculator

---
+++ AssignmentApp/TicketPriceCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 65 || age <= 12)
+                Console.WriteLine("Ticket Price: GHC7");
+            else
+                Console.WriteLine("Ticket Price: GHC10");
+        }
+    }
+}
