2c2b67cf087e5956506074a9948f4b90d8acc172
unknown <efsomuah@st.ug.edu.gh>
2024-06-02 19:06:53 -0700

    Task 3: Added Added Triangle Type Identifier

---
+++ AssignmentApp/TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TriangleTypeIdentifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first side: ");
+            int side1 = int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter the second side: ");
+            int side2 = int.Parse(Console.ReadLine());
+            
+            Console.Write("Enter the third side: ");
+            int side3 = int.Parse(Console.ReadLine());
+
+            if (side1 == side2 && side2 == side3)
+                Console.WriteLine("Triangle Type: Equilateral");
+            else if (side1 == side2 || side2 == side3 || side1 == side3)
+                Console.WriteLine("Triangle Type: Isosceles");
+            else
+                Console.WriteLine("Triangle Type: Scalene");
+        }
+    }
+}
